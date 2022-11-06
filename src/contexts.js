f44313c8c5240be28cc9b339df082913b09a21b7
GiaoProgram <81720315+GiaoProgram@users.noreply.github.com>
2022-11-06 16:08:47 -0600

    Update contexts.js

--- src/contexts.js
+++ src/contexts.js
@@ -1,10 +1,8 @@
 import React from "react";
-
 export const ThemeContext = React.createContext({
   primaryColor: "deepskyblue",
   secondaryColor: "coral",
 });
-
 export const StateContext = React.createContext({
   state: {},
   dispatch: () => {},
