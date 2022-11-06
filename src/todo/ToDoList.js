c648b69a8b0f3ca090e9020b3de7b662e8ce09ee
GiaoProgram <81720315+GiaoProgram@users.noreply.github.com>
2022-11-06 11:32:23 -0600

    Update ToDoList.js

--- src/todo/ToDoList.js
+++ src/todo/ToDoList.js
@@ -1,43 +1,17 @@
-import ToDo from "./ToDo";
+import Todo from "./Todo";
+import { useContext } from "react";
+import { StateContext } from "../contexts";
 
-export default function TodoList({ todos = [], dispatch }) {
+export default function TodoList() {
+  const { state, dispatch } = useContext(StateContext);
+  const { todos } = state;
   return (
     <div>
       {todos.map((p, i) => (
-        <div key={p.id}>
+       <div key={p.id}>
           <ToDo {...p} />
 
-          <input
-            type="submit"
-            value="Completed"
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch({
-                type: "TOGGLE_TODO",
-                id: p.id,
-                check: p.check,
-                completed: p.completed,
-              });
-            }}
-          />
           <br />
-          <input
-            type="submit"
-            value="Delete"
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch({ type: "DELETE_TODO", id: p.id });
-            }}
-          />
-          <br />
-          <label>
-            Completed?
-            <b> {p.check ? "YES" : "NO"} </b>
-            <br />
-            Completed on
-            <b> {p.check ? p.completed : ""} </b>
-          </label>
-        </div>
       ))}
     </div>
   );
