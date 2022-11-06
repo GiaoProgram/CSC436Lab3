697a9c96a1a49c9f59973220b8c9017842076f07
GiaoProgram <81720315+GiaoProgram@users.noreply.github.com>
2022-11-06 11:30:43 -0600

    Update ToDo.js

--- src/todo/ToDo.js
+++ src/todo/ToDo.js
@@ -1,23 +1,83 @@
-import {useState} from 'react';
+import React from "react";
+import { useResource } from "react-request-hook";
 
-export default function ToDo({ title, content, author, time, dispatch, user }) {
-    const [check, setCheck]=useState(false);
-    const [completed, setCompleted]=useState("");
-    function update(){
-      setCheck(!check);
-      setCompleted(Date(Date.now()));
-    }
-    return (
-      <div>
-        <h3>{title}</h3>
-        <div>{content}</div>
+export default function ToDo({
+  title,
+  content,
+  author,
+  time,
+  completed,
+  completedOn,
+  check,
+  id,
+}) {
+
+ const [deleted, deleteTodo] = useResource((id) => ({
+    url: "/todos/" + id,
+    method: "DELETE",
+  }));
+    
+  
+  const [toggle, togglePost] = useResource(
+    ({ id, completed, completedOn }) => ({
+      url: `/posts/${id}`,
+      method: "patch",
+      data: { id, completed, completedOn },
+    })
+  );
+    
+    
+  return (
+    <div>
+      <h3 style={{ color: secondaryColor }}>{title}</h3>
+      <div>{content}</div>
+      <br />
+      <i>
+        Written by <b style={{ color: tertiaryColor }}>{author}</b>
+      </i>
+      <br />
+      Created on <b> {time}</b>
+      <br />
+      <input
+        type="submit"
+        value="Completed"
+        onClick={(event) => {
+          event.preventDefault();
+          togglePost({
+            id: id,
+            completed: !completed,
+            completedOn: Date(Date.now()),
+          });
+          dispatch({
+            type: "TOGGLE_TODO",
+            id: id,
+            check: check,
+            completed: completed,
+            completedOn: completedOn,
+          });
+        }}
+      />
+      <br />
+      <input
+        type="submit"
+        value="Delete"
+        onClick={(event) => {
+          event.preventDefault();
+          deletePost({ id: id });
+          dispatch({ type: "DELETE_TODO", id: id });
+        }}
+      />
+      <br />
+      <label>
+        Completed?
+        <b>{completed ? "yes" : "no"}</b>
         <br />
-        <i>
-          Written by <b>{author}</b>
-        </i>
+        Completed on
+        <b> {completed ? completedOn : ""}</b>
         <br />
-        Completed<b> {check ? complted:""}</b>
-        <br />
-      </div>
-    );
-  }
+      </label>
+    </div>
+  );
+}
+
+//{completed == null ? (check ? "Yes" : "No") : completed}
