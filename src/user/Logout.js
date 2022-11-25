d58df9a73f45186e0e45ae5350b8e06beb425aeb
GiaoProgram <81720315+GiaoProgram@users.noreply.github.com>
2022-11-25 03:39:50 -0600

    Update Logout.js

--- src/user/Logout.js
+++ src/user/Logout.js
@@ -1,13 +1,20 @@
-export default function Logout({ user, dispatch }) {
+import { useContext } from 'react'
+import { StateContext } from "../contexts/StateContext";
+
+export default 
+function Logout() {
+    const { state, dispatch } = useContext(StateContext);
+    const{ user } = state;
     return (
-      <form
+        <form 
         onSubmit={(e) => {
-          e.preventDefault();
-          dispatch({ type: "LOGOUT" });
-        }}
-      >
-        Logged in as: <b>{user}</b>
-        <input type="submit" value="Logout" />
-      </form>
-    );
-  }
+         e.preventDefault();
+         dispatch({ type: "LOGOUT" });
+         return null;
+      }}
+    >
+      Logged in as: <b>{user.username}</b>
+      <input type="submit" value="Logout" />
+    </form>
+  );
+}
