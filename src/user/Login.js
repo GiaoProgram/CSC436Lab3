b22cdf1040bac5b9b4624f7781c509a64484dc50
GiaoProgram <81720315+GiaoProgram@users.noreply.github.com>
2022-11-25 03:38:02 -0600

    Update Login.js

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,26 +1,70 @@
 import React, { useState } from "react";
+import { useResource } from "react-request-hook";
+import { StateContext } from "../contexts";
 
-export default function Login({ dispatch }) {
+function Login() {
   const [username, setUsername] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
+  const [password, setPassword] = useState("");
+  const { dispatch } = useContext(StateContext);
+  
+  
+function handlePassword(evt) {
+    setPassword(evt.target.value);
+  }
+
+  const [user, login] = useResource((username, password) => ({
+    url: "auth/login",
+    method: "post",
+    data: { username, password },
+  }));
+
+  useEffect(() => {
+    if (user && user.isLoading === false && (user.data || user.error)) {
+      if (user.error) {
+        setLoginFailed(true);
+      } else {
+        setLoginFailed(false);
+        dispatch({
+          type: "LOGIN",
+          username: user.data.username,
+          access_token: user.data.access_token,
+        });
+      }
+    }
+  }, [user]);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        dispatch({ type: "LOGIN", username });
-      }}
-    >
-      <label htmlFor="login-username">Username:</label>
-      <input
-        type="text"
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-        name="login-username"
-        id="login-username"
-      />
-      <label htmlFor="login-password">Password:</label>
-      <input type="password" name="login-password" id="login-password" />
-      <input type="submit" value="Login" disabled={username.length === 0} />
-    </form>
+    <>
+      {loginFailed && (
+        <span style={{ color: "red" }}>Invalid username or password</span>
+      )}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          login(username, password);
+          //dispatch({ type: "LOGIN", username });
+        }}
+      >
+        <label htmlFor="login-username">Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          name="login-username"
+          id="login-username"
+        />
+        <label htmlFor="login-password">Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePassword}
+          name="login-username"
+          id="login-username"
+        />
+
+        <input type="submit" value="Login" disabled={username.length === 0} />
+      </form>
+    </>
   );
 }
